8c32b14f78aa32f92e060fb4e38bcbffb977d052
jooaodex <joao.lucas1510@hotmail.com>
2024-03-10 04:28:21 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if(hora >= 0 && hora < 12){
+        img.src = 'manha.png'
+        document.body.style.background = '#ffdd8c'
+    }else if(hora >= 12 && hora <= 18){
+        img.src = 'tarde.png'
+        document.body.style.background = '#cc4e00'
+    }else{
+        img.src = 'noite.png'
+        document.body.style.background = '#03223f'
+    }
+}
+
